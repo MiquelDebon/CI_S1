ba553ce2e43ca388cac3f24bf190abf984aba33c
Miquel Debón Villagrasa <mdebonbcn@gmail.com>
2023-04-06 12:41:30 +0200

    Done N2

--- src/S1_N2/Main.java
+++ src/S1_N2/Main.java
@@ -5,8 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Author: Miquel Debon Villagrasa
 
         System.out.println(Input.readByte("✏️Write a Byte: "));
+
+        System.out.println(Input.readInteger("✏️Write a Integer: "));
+
+        System.out.println(Input.readFloat("✏️Write a Float: "));
+
+        System.out.println(Input.readDouble("✏️Write a Double: "));
+
+        System.out.println(Input.readChar("✏️Write a Character: "));
+
+        System.out.println(Input.readString("✏️Write a String: "));
+
+        System.out.println(Input.readYesNo("✏️Write a Y/N: "));
+
+        System.out.println(Input.inputReturnIntWhileBetweenRange(2,4,"✏️Write a integer between 2-4: "));
 
     }
 }
